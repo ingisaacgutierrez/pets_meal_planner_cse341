140c58fa68f1fef57503c03cc1282165c331f91d
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-06-24 16:27:04 -0500

    add files to git hub to start the porject

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const getName = (req, res) => {
+  res.send('New Project');
+};
+
+module.exports = { getName };
---
+++ swagger.js
@@ -0,0 +1,23 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Restaurant API'
+  },
+  //host: 'render link',
+  //schemes: ['https']
+  host: 'localhost:3030',
+  schemes: ['http']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+  await import('./routes/index.js');
+});
