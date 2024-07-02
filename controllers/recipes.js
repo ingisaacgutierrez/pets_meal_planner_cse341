313e560ab098788f2fc33513c723e5b9a4750865
Taylor Christensen <97205707+Kuleman475@users.noreply.github.com>
2024-07-01 21:47:17 -0400

    recipes updated

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,4 +1,6 @@
+const { ObjectId } = require('mongodb');
 const mongodb = require('../db/connect');
+const objectId = require('mongodb').ObjectId;
 
 const getAllRecipes = async (req, res) => {
   const result = await mongodb.getDb().db('Pets_Meal_Planner').collection('recipe').find();
@@ -8,13 +10,23 @@ const getAllRecipes = async (req, res) => {
   });
 };
 
+const getSingleRecipe = async (req, res) =>{
+  const recipeId = new objectId(req.params.id);
 
+  const result = await mongodb.getDb().db('Pets_Meal_Planner').collection('recipe').find( {__id: recipeId} );
+
+  result.toArray().then((list) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).json(list);
+  });
+};
 
 const createRecipe = async (req, res) => {
   const recipe = {
     name: req.body.name,
     ingredients: req.body.ingredients,
-    instructions: req.body.instructions
+    instructions: req.body.instructions,
+    nutrition: req.body.nutrition
   };
   const response = await mongodb.getDb().db('Pets_Meal_Planner').collection('recipe').insertOne(recipe);
   if (response.acknowledged) {
@@ -24,4 +36,37 @@ const createRecipe = async (req, res) => {
   }
 };
 
-module.exports = { getAllRecipes, createRecipe };
+const updateRecipe = async (req, res) => {
+
+  const recipeId = new objectId(req.params.id);
+
+  const recipe = {
+    name: req.body.name,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    nutrition: req.body.nutrition
+  }
+
+  const response = await mongodb.getDb().db('Pets_Meal_Planner').collection('recipe').replaceOne({__id: recipeId}, recipe);
+  if (response.acknowledged) {
+    res.status(202).json(recipe);
+  }
+  else {
+    res.status(402).json(recipe.error || "OOPS something went wrong")
+  }
+};
+
+const deleteRecipe = async (req, res) => {
+  const recipeId = new objectId(req.params.id);
+
+  const response = await mongodb.getDb().db('Pets_Meal_Planner').collection('recipe').deleteOne({__id: recipeId});
+
+  if (response.acknowledged > 0) {
+    res.status(205).json("Recipe Succesfully Deleted")
+  }
+  else {
+    res.status(405).json(response.err || "OOPS Something went wrong")
+  }
+}
+
+module.exports = { getAllRecipes, getSingleRecipe, createRecipe, updateRecipe, deleteRecipe };
